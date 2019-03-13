da06d0e6b8a534fefd6e14082d6c25cc37f18c59
lee_chen <Lee_Chen@asgard.com.tw>
2019-03-13 13:31:06 +0800

    homework-2

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,13 +1,48 @@
 package com.home.currency;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText ed_ntd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed_ntd = findViewById(R.id.ntd);
+
+    }
+
+    public void calculate(View view)
+    {
+        if (ed_ntd.getText().toString().length()==0)
+        {
+            showAlert("Problem","Please enter your NTD amount");
+        }else
+        {
+            String ntdString = ed_ntd.getText().toString();
+            float ntd = Float.parseFloat(ntdString);
+            float us = (float) (ntd/ 30.9);
+            showAlert("Result","USD is "+us);
+        }
+
+    }
+
+    private void showAlert(String title , String message)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK",null)
+                .show()
+        ;
     }
 }
