38cad99555db2f9dc88a7dd9375885b038b7a77f
lee_chen <Lee_Chen@asgard.com.tw>
2019-03-14 10:10:08 +0800

    homework-3-fix declare edittext

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private EditText ed_ntd;
+    private EditText edNtd;
 
 
     @Override
@@ -17,18 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ed_ntd = findViewById(R.id.ntd);
+        edNtd = findViewById(R.id.ntd);
 
     }
 
     public void calculate(View view)
     {
-        if (ed_ntd.getText().toString().length()==0)
+        if (edNtd.getText().toString().length()==0)
         {
             showAlert(getString(R.string.problem),getString(R.string.pleace_enter_your_ntd));
         }else
         {
-            String ntdString = ed_ntd.getText().toString();
+            String ntdString = edNtd.getText().toString();
             float ntd = Float.parseFloat(ntdString);
             float us = (float) (ntd/ 30.9);
             showAlert(getString(R.string.result),getString(R.string.usd_is)+us);
